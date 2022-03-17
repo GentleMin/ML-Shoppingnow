eb02200cdb16bb004b0420ebc59a3e3f16f89ddc
GentMush <40021781+GentleMin@users.noreply.github.com>
2022-03-17 22:13:25 +0100

    Project 1B updated;

--- Project0/JTM/task0.py
+++ Project0/JTM/task0.py
@@ -4,27 +4,71 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression, HuberRegressor
 
 
+"""I. Linear model, with intercept"""
+
 # 1. Training
 
 # Load data
 pd_train = pd.read_csv("./train.csv")
-X = np.asarray(pd_train.iloc[:, 2:])
-y = np.asarray(pd_train.y)
+X = pd_train.iloc[:, 2:]
+y = pd_train.y
 
 # Train data
 predictor = LinearRegression(fit_intercept=True).fit(X, y)
 
-# Diagnostics
+# Training diagnostics
 y_est = predictor.predict(X)
 rms_misfit = np.sqrt(np.mean(np.abs(y - y_est)**2))
 wt = predictor.coef_
 intercept = predictor.intercept_
+
+print("Linear model, with intercept ----------------------")
 print(f"RMS-misfit = {rms_misfit}")
 print(f"Intercept = {intercept}; Linear coefficients: {wt}")
 
 # 2. Testing & Predicting
 pd_test = pd.read_csv("./test.csv")
-y_test = predictor.predict(np.asarray(pd_test.iloc[:, 1:]))
+y_test = predictor.predict(pd_test.iloc[:, 1:])
 
 pd_output = pd.DataFrame(data={"Id": pd_test.Id, "y": y_test})
-pd_output.to_csv("./output.csv", index=False)
+pd_output.to_csv("./result_linear_intercept.csv", index=False)
+
+
+"""II. Linear model, without intercept"""
+
+# 1. Training
+
+# Load data
+pd_train = pd.read_csv("./train.csv")
+X = pd_train.iloc[:, 2:]
+y = pd_train.y
+
+# Train data
+predictor = LinearRegression(fit_intercept=False).fit(X, y)
+
+# Training diagnostics
+y_est = predictor.predict(X)
+rms_misfit = np.sqrt(np.mean(np.abs(y - y_est)**2))
+wt = predictor.coef_
+
+print("Linear model, without intercept -------------------")
+print(f"RMS-misfit = {rms_misfit}")
+print(f"Intercept = {intercept}; Linear coefficients: {wt}")
+
+# 2. Testing & Predicting
+pd_test = pd.read_csv("./test.csv")
+y_test = predictor.predict(pd_test.iloc[:, 1:])
+
+pd_output = pd.DataFrame(data={"Id": pd_test.Id, "y": y_test})
+pd_output.to_csv("./result_linear.csv", index=False)
+
+
+"""II. Simply taking the main value"""
+
+# 2. Testing & Predicting
+pd_test = pd.read_csv("./test.csv")
+y_test = np.mean(pd_test.iloc[:, 1:], axis=1)
+
+pd_output = pd.DataFrame(data={"Id": pd_test.Id, "y": y_test})
+pd_output.to_csv("./result_mean.csv", index=False)
+
---
+++ Project1B/JTM/Project_1B.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import RidgeCV, LassoCV, HuberRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+"""Preliminary: feature map"""
+def map_to_feature(X, feature_map, max_n):
+    Phi = np.stack([feature_map(X, n) for n in range(max_n)], axis=1)
+    return Phi
+
+feature_map_list = [
+    lambda X, n: X.iloc[:,n],
+    lambda X, n: X.iloc[:,n]**2,
+    lambda X, n: np.exp(X.iloc[:,n]),
+    lambda X, n: np.cos(X.iloc[:,n]),
+    lambda X, n: np.ones(X.iloc[:,n].shape)
+]
+feature_len_list = [5, 5, 5, 5, 1]
+
+
+"""1. Train Data"""
+df_train = pd.read_csv("./train.csv")
+X = df_train.iloc[:,2:]
+y = df_train.iloc[:,1]
+Phi = np.concatenate([map_to_feature(X, feature_map_list[i], feature_len_list[i]) for i in range(len(feature_len_list))], axis=1)
+
+print("Condition number without regularization: {}".format(np.linalg.cond(Phi.T @ Phi)))
+
+# cross-validate regularization strength
+alphas = np.logspace(-1, 2, num=31)
+predictor = RidgeCV(alphas=alphas, cv=10, fit_intercept=False).fit(Phi, y)
+rms_err = np.sqrt(np.mean((y - predictor.predict(Phi))**2))
+alpha_chosen = predictor.alpha_
+
+# training diagnostics
+print("RMS-error:  {}".format(rms_err))
+print("Reg choice: {}".format(alpha_chosen))
+print("Condition:  {}".format(np.linalg.cond(Phi.T @ Phi + alpha_chosen * np.identity(Phi.shape[1]))))
+print(predictor.coef_)
+
+"""2. Optional: visualize residual, and decide whether to use outlier-robust method"""
+sns.histplot(y - predictor.predict(Phi))
+plt.show()
+
+predictor = HuberRegressor(alpha=alpha_chosen, fit_intercept=False).fit(Phi, y)
+rms_err = np.sqrt(np.mean((y - predictor.predict(Phi))**2))
+
+# training diagnostics
+print("RMS-error:  {}".format(rms_err))
+print(predictor.coef_)
+
+# output
+with open("./results_Ridge-CV10-Huber.csv", 'w') as fwrite:
+    for ci in predictor.coef_:
+        print(ci, file=fwrite)
+
